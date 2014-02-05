8bcb3b5a79647ef9abf4eb7c6b662a8c3403e2ac
ttitto <ttitto_prog@vfemail.net>
2014-02-04 18:49:31 -0800

    ChromeSpy for .Net 3.5 threading Tasks not finished

---
+++ ChromeSpy/ChromeSpy/ChromeSpy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BrowserSpy;
+
+namespace ChromeSpy
+{
+    class ChromeSpyClass
+    {
+        static void Main(string[] args)
+        {
+           var chromeUrls= ChromeWay.GetChromeUrl();
+           foreach (var url in chromeUrls)
+           {
+               Console.WriteLine(url);
+           }
+
+        }
+    }
+}
